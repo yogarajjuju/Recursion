6ed42b5abe2efde7e3d44b5ac6cdc75ae2bd2c0b
Yogaraj <yogarajjuju22@gmail.com>
2025-05-24 18:56:17 +0530

    Bankai

---
+++ src/Message.java
@@ -0,0 +1,27 @@
+public class Message {
+    public static void main(String[] args) {
+        message();
+        //write a function to print hello world 5 times
+    }
+    public static void message(){
+        System.out.println("Hello World");
+        message1();
+    }
+    public static void message1(){
+        System.out.println("Hello World");
+        message2();
+    }
+    public static void message2(){
+        System.out.println("Hello World");
+        message3();
+    }
+    public static void message3(){
+        System.out.println("Hello World");
+        message4();
+    }
+    public static void message4(){
+        System.out.println("Hello World");
+    }
+
+
+}
