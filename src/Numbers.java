b76bc1a240783e6bbd7873f844b19b91b5329bd2
Yogaraj <yogarajjuju22@gmail.com>
2025-05-31 11:40:26 +0530

    Bankai

---
+++ src/Numbers.java
@@ -0,0 +1,24 @@
+public class Numbers {
+    public static void main(String[] args) {
+        number1(1);
+    }
+    static void number1(int n){
+        System.out.println(n);
+        number2(2);
+    }
+    static void number2(int n){
+        System.out.println(n);
+        number3(3);
+    }
+    static void number3(int n){
+        System.out.println(n);
+        number4(4);
+    }
+    static void number4(int n){
+        System.out.println(n);
+        number5(5);
+    }
+    static void number5(int n){
+        System.out.println(n);
+    }
+}
---
+++ src/Sample.java
@@ -0,0 +1,14 @@
+public class Sample {
+    public static void main(String[] args) {
+        print(1);
+    }
+    public static  void print(int n ){
+        if(n==5){
+            System.out.println(5);
+            return;
+        }
+        System.out.println(n);
+        print(n+1);
+
+    }
+}
