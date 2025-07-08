32980eaf502268cd4e3d6f9aefcece892da2ebca
Yogaraj <yogarajjuju22@gmail.com>
2025-07-08 15:07:12 +0530

    Bankai

--- src/String/Stream.java
+++ src/String/Stream.java
@@ -4,7 +4,8 @@ import java.util.*;
 public class Stream {
     public static void main(String[] args) {
         String str = "baachda";
-        skip(" ", str);
+//        skip(" ", str);
+        System.out.println(skip(str));
 
     }
     static void skip(String p , String up){
@@ -17,6 +18,18 @@ public class Stream {
             skip(p,up.substring(1));
         } else{
             skip(p+ch ,up.substring(1));
+        }
+    }
+    static String skip(String up){// using a single augments 
+        if(up.isEmpty()){
+           return "";
+
+
+        } char ch = up.charAt(0);
+        if(ch=='a'){
+           return  skip(up.substring(1));
+        } else{
+           return ch+skip(up.substring(1));
         }
     }
 }
