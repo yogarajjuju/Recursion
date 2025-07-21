42e79c0b964301af4ad237e16b1a71218b904b23
Yogaraj <yogarajjuju22@gmail.com>
2025-07-21 16:26:00 +0530

    Bankai

---
+++ src/Arrays/PathObst.java
@@ -0,0 +1,31 @@
+package Arrays;
+import java.util.*;
+
+public class PathObst {
+    public static void main(String[] args) {
+       boolean maze[][]= {{true,true,true},
+               {true,false,true},
+               {true,true,true}
+
+       };
+       path("",maze,0,0);
+
+    }
+    static void path(String p , boolean [][]maze, int r , int c){
+        if(r==maze.length-1 && c==maze[0].length-1) {
+            System.out.println(p);
+            return;
+        }
+            if(!maze[r][c]){
+                return;
+            }
+
+
+        if(r<maze.length-1){
+            path(p+'D',maze,r+1,c);
+        }
+        if(c<maze[0].length-1){
+            path(p+'R',maze,r,c+1);
+        }
+    }
+}
