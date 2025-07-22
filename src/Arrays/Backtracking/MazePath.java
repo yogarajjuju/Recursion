02dd3fb6534b95e444b0eb797edbe656f5769424
yogarajjuju <yogarajjuju22@gmail.com>
2025-07-22 11:15:56 +0530

    Bankai

---
+++ src/Arrays/Backtracking/MazePath.java
@@ -0,0 +1,22 @@
+package Arrays.Backtracking;
+import java.util.*;
+
+
+public class MazePath {
+    public static void main(String[] args) {
+        path("",3,3);
+
+    }
+    static void path(String p, int r, int c ){
+        if(r==1&&c==1){
+            System.out.println(p);
+            return;
+        }
+        if(r>1){
+            path(p+'D',r-1,c);
+        }
+        if(c>1){
+            path(p+'R',r,c-1);
+        }
+    }
+}
---
+++ src/Arrays/Backtracking/MazePathlist.java
@@ -0,0 +1,25 @@
+package Arrays.Backtracking;
+
+import java.util.*;
+public class MazePathlist {
+    public static void main(String[] args) {
+        System.out.println(path("",3,3));
+
+    }
+    static ArrayList<String>path(String p, int r , int c){
+        if(r==1&&c==1){
+            ArrayList<String> list = new ArrayList<>();
+            list.add(p);
+            return list;
+        }
+        ArrayList<String> list = new ArrayList<>();
+        if(r>1){
+            list.addAll(path(p+'D',r-1,c));
+
+        }
+        if(c>1){
+            list.addAll(path(p+'R',r,c-1));
+        }
+        return list;
+    }
+}
