f5b838a3c3b326d6d48431fd061ab0eef91ba3ea
yogarajjuju <yogarajjuju22@gmail.com>
2025-07-22 11:29:32 +0530

    Bankai

--- src/Arrays/Sorted.java
+++ src/Arrays/Sorted.java
@@ -12,4 +12,57 @@ public class Sorted {
         return(arr[index]<arr[index+1])&& isSorted(arr,index+1);
 
     }
+
+    public static class Allpath {
+        public static void main(String[] args) {
+
+        }
+        static void allpath(String p ,boolean[][]maze, int r,int c){
+            if(r== maze.length-1&&c==maze[0].length-1){
+                System.out.println(p);
+                return;
+            }
+            if(r< maze.length-1){
+                allpath(p+'D',maze, r+1,c);
+            }
+            if(c<maze[0].length-1){
+                allpath(p+'R',maze,r,c+1);
+            }
+            if(r>0){
+                allpath(p+'U',maze,r-1,c);
+            }
+            if(c>0){
+                allpath(p+'L',maze,r,c-1);
+            }
+        }
+    }
+
+    public static class PathObst {
+        public static void main(String[] args) {
+           boolean maze[][]= {{true,true,true},
+                   {true,false,true},
+                   {true,true,true}
+
+           };
+           path("",maze,0,0);
+
+        }
+        static void path(String p , boolean [][]maze, int r , int c){
+            if(r==maze.length-1 && c==maze[0].length-1) {
+                System.out.println(p);
+                return;
+            }
+                if(!maze[r][c]){
+                    return;
+                }
+
+
+            if(r<maze.length-1){
+                path(p+'D',maze,r+1,c);
+            }
+            if(c<maze[0].length-1){
+                path(p+'R',maze,r,c+1);
+            }
+        }
+    }
 }
