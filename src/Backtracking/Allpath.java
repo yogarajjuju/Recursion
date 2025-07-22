573a25858256f9b5d6ab1d3b5f40ee45de3875fa
Yogaraj <yogarajjuju22@gmail.com>
2025-07-22 16:02:45 +0530

    Bankai

--- src/Backtracking/Allpath.java
+++ src/Backtracking/Allpath.java
@@ -7,28 +7,40 @@ public class Allpath {
                 {true,true,true}
 
         };
-        allpath("",maze,0,0);
+        allPath("",maze,0,0);
 
     }
-    static void allpath(String p ,boolean[][]maze, int r,int c){
-        if(r== maze.length-1&&c==maze[0].length-1){
+    static void allPath(String p ,boolean[][]maze, int r,int c){
+        if (r == maze.length - 1 && c == maze[0].length - 1) {
             System.out.println(p);
             return;
         }
-        if(!maze[r][c]){
+
+        if (!maze[r][c]) {
             return;
         }
-        if(r< maze.length-1){
-            allpath(p+'D',maze, r+1,c);
+
+        // i am considering this block in my path
+        maze[r][c] = false;
+
+        if (r < maze.length - 1) {
+            allPath(p + 'D', maze, r+1, c);
         }
-        if(c<maze[0].length-1){
-            allpath(p+'R',maze,r,c+1);
+
+        if (c < maze[0].length - 1) {
+            allPath(p + 'R', maze, r, c+1);
         }
-        if(r>0){
-            allpath(p+'U',maze,r-1,c);
+
+        if (r > 0) {
+            allPath(p + 'U', maze, r-1, c);
         }
-        if(c>0){
-            allpath(p+'L',maze,r,c-1);
+
+        if (c > 0) {
+            allPath(p + 'L', maze, r, c-1);
         }
+
+        // this line is where the function will be over
+        // so before the function gets removed, also remove the changes that were made by that function
+        maze[r][c] = true;
     }
 }
